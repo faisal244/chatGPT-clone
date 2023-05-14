fcd6ca38fc9ebf7fc9317d5b31d39fbb333fd19c
Faisal Mohammed <m.faisal244@gmail.com>
2023-05-14 18:18:38 +0100

    Set up nextAuth.js for Firebase authentication
    - Used a ternary operator to show the user a login screen if there is not a session in progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
-import SideBar from "../../components/Sidebar";
+import { Session } from "inspector";
+import SideBar from "../../components/SideBar";
 import "./globals.css";
+import { SessionProvider } from "../../components/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import Login from "../../components/Login";
 // import { Inter } from "next/font/google";
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -9,22 +14,29 @@ export const metadata = {
 	description: "A next.js Project Created by Faisal Mohammed",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const session = await getServerSession(authOptions);
 	return (
 		<html lang="en">
 			<head />
 			<body>
-				<div className="flex">
-					<div className="bg-[#202123] md:min-w-[20rem] max-w-xs overflow-y-auto h-screen">
-						<SideBar />
-					</div>
-					{/* ClientProvider - Notification */}
-					<div className="bg-[#343541] flex-1 "> {children}</div>
-				</div>
+				<SessionProvider session={session}>
+					{!session ? (
+						<Login />
+					) : (
+						<div className="flex">
+							<div className="bg-[#202123] md:min-w-[20rem] max-w-xs overflow-y-auto h-screen">
+								<SideBar />
+							</div>
+							{/* ClientProvider - Notification */}
+							<div className="bg-[#343541] flex-1 "> {children}</div>
+						</div>
+					)}
+				</SessionProvider>
 			</body>
 		</html>
 	);
