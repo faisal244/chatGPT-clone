cbd65b271f16766eea7f607a0f74c2c66feccae1
Faisal Mohammed <m.faisal244@gmail.com>
2023-05-17 23:52:56 +0100

    Created NewChat Component, connected to firebase

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,14 +1,31 @@
+"use client";
+
 import { PlusIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { db } from "../firebase";
 
 function NewChat() {
+	const router = useRouter();
+	const { data: session } = useSession();
 
-	const createNewChat = asytnc() => {
-		
-	}
+	const createNewChat = async () => {
+		const doc = await addDoc(
+			collection(db, "users", session?.user?.email!, "chats"),
+			{
+				userId: session?.user?.email!,
+				createdAt: serverTimestamp(),
+			}
+		);
+
+		router.push(`/chat/${doc.id}`);
+	};
 	return (
 		<div
 			onClick={createNewChat}
-			className="border-gray-700 border chatRow items-center justify-center">
+			className="border-gray-700 border chatRow items-center justify-center"
+		>
 			<PlusIcon className="h-4 w-4" />
 			<p className="">New Chat</p>
 		</div>
