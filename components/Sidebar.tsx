543ec8a1c90019f9580291fbe93a32306eb6da3e
Faisal Mohammed <m.faisal244@gmail.com>
2023-05-21 16:50:11 +0100

    Completed Model Selection Functionality
    - Installed react-select and SWR packages
    
    - Created getEngines.ts to handle API routes for ChatGPT Model fetching
    
    - Created a ModelSelection component that uses react-select to allow the user to view a dropdown listing all of the different models available via the OpenAI API
    
    - Also added the model selection dropdown in the chat input box so it can be seen below the text entry box when viewed on mobile devices

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import NewChat from "./NewChat";
 import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "../firebase";
 import ChatRow from "./ChatRow";
+import ModelSelection from "./ModelSelection";
 
 function SideBar() {
 	const { data: session } = useSession();
@@ -18,21 +19,30 @@ function SideBar() {
 			)
 	);
 
-	// console.log(chats);
 	return (
 		<div className="p-2 flex flex-col h-screen">
 			<div className="flex-1">
 				<div>
 					{/* NewChat */}
 					<NewChat />
-					<div>{/* ModelSelection Component goes here */}</div>
-					{/* Map through the ChatRows */}
-					{chats?.docs.map((chat) => (
-						<ChatRow
-							key={chat.id}
-							id={chat.id}
-						/>
-					))}
+					<div className="hidden sm:inline">
+						<ModelSelection />
+					</div>
+
+					<div className="flex flex-col space-y-2 my-2">
+						{loading && (
+							<div className="animate-pulse text-center text-white">
+								<p>Loading Chats...</p>
+							</div>
+						)}
+						{/* Map through the ChatRows */}
+						{chats?.docs.map((chat) => (
+							<ChatRow
+								key={chat.id}
+								id={chat.id}
+							/>
+						))}
+					</div>
 				</div>
 			</div>
 			{session && (
